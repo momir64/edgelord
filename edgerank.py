aa80593fa3634a05aadab8e263e0b8f52c16b176
Momir Stanišić <momir.stanisic.ns@gmail.com>
2023-06-15 13:26:24 +0200

    Samo dict

--- edgerank.py
+++ edgerank.py
@@ -16,18 +16,21 @@ def get_edgeranked_statuses(affinity_graph=None, user=None, statuses=None):
     if not statuses:
         info(f'█  Loading statuses')
         start_time = time.time()
-        statuses = get_statuses(as_list=True)
+        statuses = get_statuses()
         info(f'✓  {"Loading statuses:".ljust(32)}{("%.4f seconds" % (time.time() - start_time)).rjust(20)}\033[K\n')
 
-    info(f'█  Sorting statuses')
+    info(f'█  Calculating edgerank score')
     start_time = time.time()
     current_date = datetime.now()
-    for status in statuses:
+    for status in statuses.values():
         status['affinity'] = 1 if not affinity_graph or not user else (affinity_graph.get_affinity(user, status['author']) / AFFINITY_REDUCTION)
         status['weight'] = (status['reactions'] * REACTION_AFFINITY_WEIGHT + status['comments'] * COMMENT_AFFINITY_WEIGHT + status['shares'] * SHARE_AFFINITY_WEIGHT) / WEIGHT_REDUCTION
         status['time_decay'] = get_time_decay(status['date'], current_date)
         status['edgerank'] = status['affinity'] * status['weight'] * status['time_decay']
-    sorted_statuses = sorted(statuses, key=itemgetter('edgerank'), reverse=True)
-    info(f'✓  {"Sorting statuses:".ljust(32)}{("%.4f seconds" % (time.time() - start_time)).rjust(20)}\033[K\n\n')
+    info(f'✓  {"Calculating edgerank score:".ljust(32)}{("%.4f seconds" % (time.time() - start_time)).rjust(20)}\033[K\n\n')
 
-    return sorted_statuses
+    return statuses
+
+
+def sort_edgeranked_statuses(statuses):
+    return sorted(statuses.values(), key=itemgetter('edgerank'), reverse=True)
