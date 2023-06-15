78ab0d6aa4fe3c490f3efb10405c17679e48ce6f
Momir Stanišić <momir.stanisic.ns@gmail.com>
2023-06-16 01:50:35 +0200

    gotove trie

--- status_trie.py
+++ status_trie.py
@@ -1,58 +1,63 @@
+from operator import itemgetter
 import re
 
+PHRASE_WEIGHT = 15
 WORD_WEIGHT = 5
 PART_WEIGHT = 1
 
 
 class StatusTrieNode:
-    def __init__(self, char):
+    def __init__(self, char, word):
         self.char = char
-        self.counter = 0
+        self.word = word
+        self.count = 0
         self.statuses = {}
         self.children = {}
         self.is_word = False
 
 
 class StatusTrie:
-    def __init__(self):
-        self.root = StatusTrieNode('')
+    def __init__(self, statuses):
+        self.root = StatusTrieNode('', '')
+        for status in statuses.values():
+            self.add_status(status)
 
 
     def add_status(self, status):
         text = status['message'].lower()
         node = self.root
-        new_word = False
         position = 0
+        word = ''
 
         for char in text:
             if char.isalnum():
-                new_word = True
+                word += char
 
                 if char in node.children:
                     node = node.children[char]
                 else:
-                    node.children[char] = StatusTrieNode(char)
+                    node.children[char] = StatusTrieNode(char, word)
                     node = node.children[char]
 
                 if status['id'] not in node.statuses:
-                    node.statuses[status['id']] = {'parts': 0, 'words': []}
-                node.statuses[status['id']]['parts'] += 1
-            elif new_word:
-                node, position, new_word = self.new_word_helper(node, status['id'], position)
-    
+                    node.statuses[status['id']] = {'parts_count': 0, 'word_positions': []}
+                node.statuses[status['id']]['parts_count'] += 1
+            elif word:
+                node, position, word = self.__new_word_helper__(node, status['id'], position)
+
         if node.char != '':
-            self.new_word_helper(node, status['id'], position)
+            self.__new_word_helper__(node, status['id'], position)
 
 
-    def new_word_helper(self, node, status, position):
-        node.statuses[status]['words'].append(position)
-        node.statuses[status]['parts'] -= 1
+    def __new_word_helper__(self, node, status, position):
+        node.statuses[status]['word_positions'].append(position)
+        node.statuses[status]['parts_count'] -= 1
         node.is_word = True
-        node.counter += 1
-        return self.root, position + 1, False
+        node.count += 1
+        return self.root, position + 1, ''
 
 
-    def get_node(self, word):
+    def __get_node__(self, word):
         node = self.root
         for char in word:
             if char in node.children:
@@ -64,24 +69,78 @@ class StatusTrie:
 
     def search(self, prompt):
         statuses = {}
-        words = re.sub(r'\s\s+', ' ', re.sub(r'[^\w\s]+', ' ', prompt)).strip().lower().split()
+        prompt += '"' if prompt.count('"') % 2 else ''
+        phrases = re.findall('"([^"]*)"', prompt)
+        prompt = re.sub('("[^"]*")', '', prompt)
 
+        for phrase in phrases:
+            statuses_phrase = self.__search_phrase__(phrase)
+            for id in statuses_phrase:
+                statuses[id] = statuses.setdefault(id, 0) + statuses_phrase[id]
+
+        statuses_normal = self.__search_normal__(prompt)
+        for id in statuses_normal:
+            statuses[id] = statuses.setdefault(id, 0) + statuses_normal[id]
+
+        return statuses, phrases + (prompt.split() if prompt != '' else [])
+
+
+    def __search_normal__(self, prompt):
+        statuses = {}
+        words = re.sub(r'\s\s+', ' ', re.sub(r'[^\w\s]+', ' ', prompt)).strip().lower().split()
         if not len(words):
             return {}
 
         for word in words:
-            node = self.get_node(word)
+            node = self.__get_node__(word)
             if node == None or node.char == '':
                 continue
-            for status, value in node.statuses.items():
-                statuses[status] = statuses.setdefault(status, 0) + len(value['words']) * WORD_WEIGHT + value['parts'] * PART_WEIGHT
+            for id, value in node.statuses.items():
+                statuses[id] = statuses.setdefault(id, 0) + len(value['word_positions']) * WORD_WEIGHT + value['parts_count'] * PART_WEIGHT
 
         return statuses
 
 
+    def __search_phrase__(self, phrase):
+        statuses = {}
+        words = re.sub(r'\s\s+', ' ', re.sub(r'[^\w\s]+', ' ', phrase)).strip().lower().split()
+        if not len(words):
+            return {}
 
-    def search_phrase(self, phrase):
-        pass
+        for i, word in enumerate(words):
+            node = self.__get_node__(word)
+            if node == None or node.char == '':
+                return {}
+            if not i:
+                for id, value in node.statuses.items():
+                    statuses[id] = value['word_positions']
+            else:
+                statuses_tmp = {}
+                for id in statuses:
+                    positions = []
+                    if id in node.statuses:
+                        for position in statuses[id]:
+                            if position + 1 in node.statuses[id]['word_positions']:
+                                positions.append(position + 1)
+                    if positions:
+                        statuses_tmp[id] = positions
+                statuses = statuses_tmp
+
+        for id, value in statuses.items():
+            statuses[id] = len(value) * PHRASE_WEIGHT
+
+        return statuses
+
+
+    def __dfs__(self, node):
+        words = [(node.word, node.count)] if node.is_word else []
+        for child in node.children.values():
+            words += self.__dfs__(child)
+        return words
+
 
     def get_suggestion(self, prefix):
-        pass
+        node = self.__get_node__(prefix)
+        if node == None or node.char == '':
+            return ''
+        return [t[0] for t in sorted(self.__dfs__(node), key=itemgetter(1), reverse=True)]
