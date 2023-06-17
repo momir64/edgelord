89078ac8a2b988a6cf6b0d08660a29c94f5a8a9c
Momir Stanišić <momir.stanisic.ns@gmail.com>
2023-06-17 14:03:17 +0200

    comment

--- time_decay.py
+++ time_decay.py
@@ -1,6 +1,6 @@
 import math
 
-# vraća koeficijent između 0.1 i 10 za opseg od 0 do 30 dana
+# vraća koeficijent između 10 i 0.1 za opseg od 0 do 30 dana
 def get_time_decay(old_date, current_date):
     return max(0.1, min(10, 5 - math.log(max(1, (current_date - old_date).total_seconds()) / 86400) * 1.4))   # exponential
     return max(0.1, min(10, 10 - (current_date - old_date).total_seconds() / 259200))                         # linear
